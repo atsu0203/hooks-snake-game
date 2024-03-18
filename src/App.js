8a90cd499c79c5f133aee7de67bccc789dfc8a7c
atsu0203 <atsu0203@gmail.com>
2024-03-19 02:53:31 +0900

    add  Button(goUp)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {  useEffect, useState } from 'react';
 import Navigation from './components/Navigation';
 import Field from './components/Field';
 import Button from './components/Button';
 import ManipulationPanel from './components/ManipulationPanel';
-import './App.css';
+import { initFields } from './utils'
+
+const initialPosition = { x: 17, y: 17 }
+const initialValues = initFields(35, initialPosition)
 
 function App() {
+  const [fields, setFields] = useState(initialValues)
+  const [position, setPosition] = useState()
+
+  useEffect(() => {
+    setPosition(initialPosition)
+  }, [])
+
+  const goUp = () => {
+    const { x, y } = position
+    const nextY = Math.max(y -1, 0)
+    fields[y][x] = ''
+    fields[nextY][x] = 'snake'
+    setPosition({ x, y: nextY })
+    setFields(fields)
+  }
+
+
   return (
     <div className="App">
-       <header className="header">
-         <div className="title-container">
-           <h1 className="title">Snake Game</h1>
+      <header className="header">
+        <div className="title-container">
+          <h1 className="title">Snake Game</h1>
          </div>
          <Navigation />
-       </header>
-       <main className="main">
-         <Field />
-       </main>
-       <footer className="footer">
+      </header>
+      <main className="main">
+        <Field fields={fields} />
+      </main>
+      <div style={{ padding: '16px' }}>
+        <button>進む</button>
+      </div>
+      <footer className="footer">
          <Button />
          <ManipulationPanel />
-       </footer>
+      </footer>
     </div>
   );
 }
